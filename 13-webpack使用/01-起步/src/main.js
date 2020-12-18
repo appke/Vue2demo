0fd9518f2b317a4ef39b6b0d7052a474b939983b
appke <liangmu.gdut@gmail.com>
2020-12-18 15:27:27 +0800

    webpack起步

---
+++ 13-webpack使用/01-起步/src/main.js
@@ -0,0 +1,9 @@
+const {add, mul} = require('./math.js')
+
+import {name, age} from './info'
+
+console.log(add(20, 30))
+console.log(mul(20, 30))
+
+console.log('name --- ', name)
+console.log('age  --- ', age)
