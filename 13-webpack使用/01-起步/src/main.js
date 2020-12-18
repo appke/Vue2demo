8bef3b30cd3639321c1de6cbb972623ea136f386
appke <liangmu.gdut@gmail.com>
2020-12-18 15:37:11 +0800

    ES6模块化

--- 13-webpack使用/01-起步/src/main.js
+++ 13-webpack使用/01-起步/src/main.js
@@ -1,9 +1,9 @@
+// CommonJS模块化规范
 const {add, mul} = require('./math.js')
-
-import {name, age} from './info'
-
 console.log(add(20, 30))
 console.log(mul(20, 30))
 
+// ES6模块化规范
+import {name, age} from './info'
 console.log('name --- ', name)
 console.log('age  --- ', age)
