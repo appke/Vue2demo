20b789d32a3ab97d67bea8cdc7d6b33841c59d5f
appke <liangmu.gdut@gmail.com>
2020-12-18 18:41:49 +0800

    图片资源

--- 13-webpack使用/02-配置/webpack.config.js
+++ 13-webpack使用/02-配置/webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   output: {
     // 绝对路径
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'dist/'
   },
   module: {
     rules: [
@@ -16,6 +17,18 @@ module.exports = {
       {
         test: /\.less$/,
         use: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: 'img/[name].[hash:8].[ext]'
+            },
+          }
+        ]
       }
     ]
   }
