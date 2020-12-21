1f3ec44fd78ecf857df851912eb456313f9dc6d7
appke <liangmu.gdut@gmail.com>
2020-12-21 22:57:30 +0800

    开始配置分离

---
+++ 13-webpack使用/05-配置分离/src/vue/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <p class="desc">{{ desc }}</p>
+    <button @click="btnClick">按钮</button>
+
+    呵额 --- 
+
+    <Cpn></Cpn>
+
+  </div>
+</template>
+
+
+<script>
+import Cpn from "./Cpn.vue"
+
+export default {
+  name: "App",
+  components: {
+    Cpn,
+  },
+  data() {
+    return {
+      message: "Hellow WebPack app",
+      desc: "第一次开始vue页面，哈哈哈。出新手村",
+    };
+  },
+  methods: {
+    btnClick() {
+      console.log("btnClick ----- ")
+    },
+  },
+};
+</script>
+
+<style scoped>
+.desc {
+  color: green;
+}
+</style>
