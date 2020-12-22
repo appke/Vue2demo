b1c22799fbac1ca786d301820c495f13f2bf7eff
appke <liangmu.gdut@gmail.com>
2020-12-22 17:55:12 +0800

    路由结构

--- 16-路由router/vuerouter/src/router/index.js
+++ 16-路由router/vuerouter/src/router/index.js
@@ -1,15 +1,16 @@
+import VueRouter from 'vue-router'
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+// 1.通过Vue.use(插件)，安装插件
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+// 2.创建VueRouter对象
+const routes = [
+]
+const router = new VueRouter({
+  // 配置路由和组件之间的映射关系
+  router
 })
+
+// 3.将router对象传入到Vue实例中 
+export default router
