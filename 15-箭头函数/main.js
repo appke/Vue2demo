169ef3b3fbaf6fa950b0baec3750aa0412905521
appke <liangmu.gdut@gmail.com>
2020-12-22 16:39:27 +0800

    箭头函数中的this

--- 15-箭头函数/main.js
+++ 15-箭头函数/main.js
@@ -6,5 +6,20 @@ console.log(sum(3, 8))
 
 
 // 1个参数
+const power = num => {
+  return num * num
+}
+const power2 = num => num*num
+console.log(power(9))
+console.log(power2(10))
 
 
+// 没有参数
+const test = () => {
+  console.log('Hello World!')
+  console.log('Arrow Function')
+}
+const test2 = () => console.log('Arrow Function')
+test()
+// log执行后返回结果 undefined
+console.log(test2())
