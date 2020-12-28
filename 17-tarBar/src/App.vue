4c2b57aaef15c1666c6a9f43bc71c6ef707f17e5
appke <liangmu.gdut@gmail.com>
2020-12-28 21:48:17 +0800

    开始tabbar

--- 17-tarBar/src/App.vue
+++ 17-tarBar/src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
+    <div id="tab-bar">
+      <div class="tab-bar-item">首页</div>
+      <div class="tab-bar-item">分类</div>
+      <div class="tab-bar-item">购物车</div>
+      <div class="tab-bar-item">我的</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
+@import "./assets/css/base.css";
+
+#tab-bar {
+  display: flex;
+}
+
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
 </style>
