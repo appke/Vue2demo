d512c0d4896d9f2663c65db59e0437e8d89e27f1
appke <liangmu.gdut@gmail.com>
2021-06-11 16:55:32 +0800

    update: 封装tabbarItem

--- 17-tarBar/src/App.vue
+++ 17-tarBar/src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div id="app">
     <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="" alt=""/>
+        <text slot="item-text">首页</text>
+      </tab-bar-item>
+      
     </tab-bar>
   </div>
 </template>
 
 <script>
 import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
 export default {
   name: "App",
   components: {
     TabBar,
+    TabBarItem
   }
 };
 </script>
 
 <style>
 @import "./assets/css/base.css";
+
 
 </style>
--- 17-tarBar/src/components/tabbar/TabBar.vue
+++ 17-tarBar/src/components/tabbar/TabBar.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "TabBar"
-
-}
+  name: "TabBar",
+};
 </script>
 
 <style scoped>
 #tab-bar {
   display: flex;
   background-color: #f6f6f6;
-  
+
   position: fixed;
   left: 0;
   right: 0;
   bottom: 0;
-  box-shadow: 0 -2px 1px rgba(100, 100, 100, .1);
+  box-shadow: 0 -2px 1px rgba(100, 100, 100, 0.1);
 }
 
 .tab-bar-item {
