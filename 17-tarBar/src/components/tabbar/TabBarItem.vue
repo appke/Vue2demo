2cd3c79925786ba74ff30e4a0a6a7f5c0dc3eb3c
appke <liangmu.gdut@gmail.com>
2021-06-16 08:36:30 +0800

    update: 搭建路由框架

--- 17-tarBar/src/components/tabbar/TabBarItem.vue
+++ 17-tarBar/src/components/tabbar/TabBarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tab-bar-item">
+  <div class="tab-bar-item" @click="itemClick">
     <div v-if="!isActive"><slot name="item-icon"></slot></div>
     <div v-else><slot name="item-icon-active"></slot></div>
     <div :class="{ active: isActive }"><slot name="item-text"></slot></div>
@@ -9,11 +9,19 @@
 <script>
 export default {
   name: "TabBarItem",
+  props: {
+    path: String
+  },
   data() {
     return {
       isActive: false,
     };
   },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    },
+  }
 };
 </script>
 
--- 17-tarBar/src/router/index.js
+++ 17-tarBar/src/router/index.js
@@ -1,13 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 如果你的改了push还是没有生效，可以考虑改replace方法
+// 修改路由replace方法,阻止重复点击报错
+const originalReplace = Router.prototype.replace;
+Router.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err);
+};
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: "",
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      // component: () => import('@/pages/login')
+      component: () => import('../views/home/Home.vue')
+    },
+    {
+      path: '/category',
+      component: () => import('../views/category/Category.vue')
+    },
+    {
+      path: '/shopcart',
+      component: () => import('../views/shopcart/Shopcart.vue')
+    },
+    {
+      path: '/profile',
+      component: () => import('../views/profile/Profile.vue')
     }
-  ]
+  ],
+  mode: 'history'
 })
