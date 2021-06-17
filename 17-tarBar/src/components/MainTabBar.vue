782229022c315a963ec255cc7ad5637804f45630
appke <liangmu.gdut@gmail.com>
2021-06-17 14:16:37 +0800

    add: Promise 使用

--- 17-tarBar/src/components/MainTabBar.vue
+++ 17-tarBar/src/components/MainTabBar.vue
@@ -9,26 +9,26 @@
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item path="/category" activeColor="#F33C65">
-      <img slot="item-icon" src="～assets/img/tabbar/category.svg" alt="" />
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="" />
       <img
         slot="item-icon-active"
-        src="～assets/img/tabbar/category_active.svg"
+        src="~assets/img/tabbar/category_active.svg"
       />
       <div slot="item-text">分类</div>
     </tab-bar-item>
     <tab-bar-item path="/shopcart" activeColor="#F33C65">
-      <img slot="item-icon" src="～assets/img/tabbar/shopcart.svg" alt="" />
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="" />
       <img
         slot="item-icon-active"
-        src="～assets/img/tabbar/shopcart_active.svg"
+        src="~assets/img/tabbar/shopcart_active.svg"
       />
       <div slot="item-text">购物车</div>
     </tab-bar-item>
     <tab-bar-item path="/profile" activeColor="#F33C65">
-      <img slot="item-icon" src="～assets/img/tabbar/profile.svg" alt="" />
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="" />
       <img
         slot="item-icon-active"
-        src="～assets/img/tabbar/profile_active.svg"
+        src="~assets/img/tabbar/profile_active.svg"
       />
       <div slot="item-text">我的</div>
     </tab-bar-item>
